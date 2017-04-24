d2e0b02cd9575b54f8614df3c36d727bba5ee588
HerrVey <handres670@gmail.com>
2017-04-24 17:58:59 +0200

    Projet Qt

--- main.cpp
+++ main.cpp
@@ -1,22 +1,9 @@
-// Imagine++ project
-// Project:  Tp6
-// Author:   Lucas
-// Date:     2016/11/16
-
-#include <Imagine/Graphics.h>
-using namespace Imagine;
 #include <iostream>
+
 using namespace std;
 
-
-#include "tools.h"
-#include "vector.h"
-#include "tile.h"
-
-int main(){
-
-    openWindow(width * bloc, height * bloc);
-
-    endGraphics();
+int main(int argc, char *argv[])
+{
+    cout << "Hello World!" << endl;
     return 0;
 }
