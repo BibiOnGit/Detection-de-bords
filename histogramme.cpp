d6642927ea6a539a7fd431978c8b8b7717c81f9d
BibiOnGit <davidsu242@gmail.com>
2017-05-04 11:10:56 +0200

    nouvel histogramme
    ecriture histo, gradient et Hc

--- histogramme.cpp
+++ histogramme.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 
 
-void gradient (unsigned char * u, float * grad, int h, int w){
+void gradient (unsigned char * u, unsigned char * grad, int h, int w){
     for (int i=0; i<h; i++){
         for (int j=0; j<w; j++){
             if(i==0 || j==0){
@@ -10,28 +10,27 @@ void gradient (unsigned char * u, float * grad, int h, int w){
             } else{
                 short int a = u[i*w + j] + u[i*w + j - 1] - u[i*w + j - w] - u[i*w + j - w - 1];
                 short int b = u[i*w + j] + u[i*w + j - w] - u[i*w + j - w -1] - u[i*w + j - 1];
-                grad[i*w + j] = std::sqrt(a*a + b*b);
+                grad[i*w + j] = (a*a + b*b);
             }
         }
     }
 }
 
-float histo (unsigned char * u, int h, int w, float mu){
-    float res;
-    float* grad = new float[w*h];
-    gradient (u, grad, h, w);
-    int sup_seuil = 0;
-    int total = 0;
-    for (int i=0; i<h; i++){
-        for (int j=0; j<w; j++){
-            if (grad[i*w + j] > mu){
-                sup_seuil ++;
-            }
-            if (grad[i*w + j] > 0){
-                total ++;
-            }
+void histo (int hist[130051], unsigned char * grad, int h, int w){// le 130051 provient du nombre total de valeurs de gradients
+    for( int i=0;i<h;i++){
+        for(int j=0;j<w;j++){
+            hist[grad[i*w+j]]++;
         }
     }
-    res = sup_seuil/total;
-    return res;
+    for(int i=130050;i>=0;i--){
+        hist[i]+=hist[i+1];
+    }
+
+}
+
+double Hc(int mu){
+    if (mu==130051){
+        return 0;
+    }
+    return double(hist[mu+1])/double(hist[1]);
 }
